4beca7e457683bc315a974298b307733c8789532
AbdelfatahMo <b5b71999@outlook.com>
2023-07-26 15:17:04 +0300

    Reference field & Handle widget operation model

--- models/operation.py
+++ models/operation.py
@@ -1,23 +1,33 @@
 from odoo import fields, models, api
 
+
 class HospitalOperation(models.Model):
     _name = 'hospital.operation'
     _description = 'Hospital Operation'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _log_access = False
     _rec_name = "operation_name"
-    
-    operation_name=fields.Char(
+    _order="sequence,id"
+
+    operation_name = fields.Char(
         string="Name"
     )
-    
+
     doctor_id = fields.Many2one(
         comodel_name="res.users",
         string="Doctor"
     )
     
+    reference_record=fields.Reference(
+        selection=[("hospital.appointment","Appointment"),
+                   ("hospital.patient","Patient")],
+        string="Record"   
+    )
+
+    sequence = fields.Integer(
+        default=10,
+    )
+
     @api.model
     def name_create(self, name):
-        return self.create({'operation_name':name}).name_get()[0]
-    
-    
+        return self.create({'operation_name': name}).name_get()[0]
